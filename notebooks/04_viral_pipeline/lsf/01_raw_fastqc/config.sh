5c5826c8d1ab1b4a4c49d42900d7c65329d9fed6
Mery Touceda-Suárez <maria.touceda.suarez@gmail.com>
2025-12-02 15:06:04 -0500

    parameters in config

--- notebooks/04_viral_pipeline/lsf/01_raw_fastqc/config.sh
+++ notebooks/04_viral_pipeline/lsf/01_raw_fastqc/config.sh
@@ -2,6 +2,7 @@ export ID=MY_ID
 export IN_LIST="/your/path/to/repo/hpc_viral_inference_pipeline/data/sra_accessions.txt"  # Path to the SRA accessions file
 export IN_DIR="/path/to/raw_reads"                                                      # Directory containing raw reads
 export FASTQC_SIF="/path/to/container/image"                                             # Path to the FastQC container
+export BIND="--bind /rs1:/rs1"  # Bind path for Apptainer, this is specific to your HPC environment
 
 # Create output directory if it doesn't exist
 mkdir -p /path/to/working/dir/01_raw_fastqc_results
